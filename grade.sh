8ddb5f8a16436c832d551f51299755dcdccba9c0
dhvyas7 <130088994+dhvyas7@users.noreply.github.com>
2023-05-25 11:23:20 -0700

    Add files via upload

---
+++ grade.sh
@@ -0,0 +1,33 @@
+rm -rf student-submission
+rm -rf grading-area
+
+mkdir grading-area
+
+git clone $1 student-submission
+echo 'Finished cloning'
+
+cd student-submission
+
+if [[ -e  ListExamples.java ]]; 
+then
+        echo "file exists"
+else
+        echo "check file path!!!"
+        exit
+fi
+
+cd ..
+
+cp student-submission/ListExamples.java grading-area
+cp *.java grading-area
+
+
+
+
+cp -r lib grading-area
+
+cd grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
